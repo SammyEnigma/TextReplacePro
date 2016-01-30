b73f16e09a2a83fc10021ddd2f4569de2ce164be
Alexander Almaleh <sashoalm@gmail.com>
2016-01-30 22:34:08 +0200

    First working version.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QRegExp>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,33 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString input = ui->plainTextEditInput->toPlainText();
+    QString output = input;
+    QString regexes = ui->plainTextEditRegex->toPlainText();
+    foreach (QString line, regexes.split('\n')) {
+        QStringList s = line.split("===");
+        if (s.size() != 2) {
+            return;
+        }
+
+        QRegExp regex(s[0]);
+        if (!regex.isValid()) {
+            return;
+        }
+
+        QString after(s[1]);
+
+        s = output.split('\n');
+        output.clear();
+        foreach (QString line, s) {
+            line.replace(regex, after);
+            output += line + '\n';
+        }
+    }
+
+    ui->plainTextEditOutput->setPlainText(output);
 }
